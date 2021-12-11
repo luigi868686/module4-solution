ac263db3835ed5930c7616d51b9dad3fbc276d7e
luigi868686 <90272762+luigi868686@users.noreply.github.com>
2021-12-11 09:30:09 +0100

    Create SpeakHello.js

---
+++ SpeakHello.js
@@ -0,0 +1,9 @@
+
+(function(window){
+  var helloSpeaker = {};
+  var speakWord = "Hello";
+  helloSpeaker.speak = function (name) {
+    return (speakWord + " " + name);
+  }
+  window.helloSpeaker = helloSpeaker;
+})(window);
