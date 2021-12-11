26186aec3e0a3f3e36928a4c69f8a52c1fa75910
luigi868686 <90272762+luigi868686@users.noreply.github.com>
2021-12-11 09:36:21 +0100

    Delete SpeakHello.js

--- SpeakHello.js
+++
@@ -1,8 +0,0 @@
-(function(window){
-  var helloSpeaker = {};
-  var speakWord = "Hello";
-  helloSpeaker.speak = function (name) {
-    return (speakWord + " " + name);
-  }
-  window.helloSpeaker = helloSpeaker;
-})(window);
